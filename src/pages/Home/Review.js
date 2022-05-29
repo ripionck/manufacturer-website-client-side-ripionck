203c96d42bcbc3f1583bf448e23218a3cee7633e
Your Name <you@example.com>
2022-05-29 17:26:17 +0600

    rating aadded

--- src/pages/Home/Review.js
+++ src/pages/Home/Review.js
@@ -2,18 +2,45 @@ import React from "react";
 
 const Review = () => {
   return (
-    <div class="card w-96 bg-base-100 shadow-xl">
-      <div class="card-body">
-        <div class="avatar">
-          <div class="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-            <img src="https://api.lorem.space/image/face?hash=3174" />
+    <div className="card lg:max-w-lg bg-base-100 shadow-xl mt-12">
+      <div className="card-body">
+        <div className="avatar flex justify-center mt-3">
+          <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+            <img src="https://api.lorem.space/image/face?hash=3174" alt="" />
           </div>
         </div>
-        <h2 class="card-title">Card title!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div class="card-actions justify-end">
-          <button class="btn btn-primary">Buy Now</button>
+        <h2 className=" text-center">Card title!</h2>
+        <div className="rating justify-center">
+          <input
+            type="radio"
+            name="rating-2"
+            className="mask mask-star-2 bg-orange-400"
+          />
+          <input
+            type="radio"
+            name="rating-2"
+            className="mask mask-star-2 bg-orange-400"
+            checked
+          />
+          <input
+            type="radio"
+            name="rating-2"
+            className="mask mask-star-2 bg-orange-400"
+          />
+          <input
+            type="radio"
+            name="rating-2"
+            className="mask mask-star-2 bg-orange-400"
+          />
+          <input
+            type="radio"
+            name="rating-2"
+            className="mask mask-star-2 bg-orange-400"
+          />
         </div>
+        <p className="text-center">
+          If a dog chews shoes whose shoes does he choose?
+        </p>
       </div>
     </div>
   );
